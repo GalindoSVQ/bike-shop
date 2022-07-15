47f84368cce3b204974062d312ec57a8576064ec
Antonio Galindo <antoniomarcos.galindo.contractor@bbva.com>
2022-07-15 09:28:30 +0200

    feat(App): create error boundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    console.error(_);
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ background: "#EBEECB", height: "100%", padding: "1rem" }}>
+          Sorry.. there was an error
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
