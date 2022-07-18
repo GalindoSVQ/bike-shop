72135c181c301ddd866f1d8c1f950dc8375ad6e3
Antonio Galindo <galindosvq@gmail.com>
2022-07-18 07:27:32 +0200

    feat(Footer): add reference content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,22 @@
+import { Pagination } from "components/Pagination";
 import { Container } from "./Footer.style";
 
 export function Footer() {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <p>
+        Handmade 🤟
+        <a href="https://gali.dev/" target="_blank" rel="noreferrer">
+          Antonio Galindo |
+        </a>
+        <a
+          href="https://github.com/GalindoSVQ/bike-shop/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          | Code
+        </a>
+      </p>
+    </Container>
+  );
 }
