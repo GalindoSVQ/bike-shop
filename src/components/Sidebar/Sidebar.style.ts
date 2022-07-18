cbcf63428e2dc7c1e7c9b141f924e8ea1cd4ddff
Antonio Galindo <galindosvq@gmail.com>
2022-07-18 07:26:00 +0200

    style(Sidebar): make it sticky

--- src/components/Sidebar/Sidebar.style.ts
+++ src/components/Sidebar/Sidebar.style.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 const Container = styled.aside`
   width: 25%;
+  position: sticky;
+  top: 0;
+  height: 100vh;
 
   @media ${({ theme }) => theme.device.mobile} {
     display: none;
