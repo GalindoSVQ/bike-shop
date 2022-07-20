cd4925e442121fd69c9eb1861314e10df5f3638d
Antonio Galindo <galindosvq@gmail.com>
2022-07-20 08:03:25 +0200

    feat(BikeCard): show retrieved values

--- src/components/BikeCard/BikeCard.tsx
+++ src/components/BikeCard/BikeCard.tsx
@@ -1,25 +1,31 @@
-import Bike from "models/bike";
 import { useCallback, useRef, useState } from "react";
+import { useShoppingCart } from "context/ShoppingCart";
 import { formatCurrency, lockScrollbar, onErrorImgHandler } from "utilities";
+import Bike from "models/bike";
 import { AddButton, Container, Data, Modal } from "./BikeCard.style";
 
 type Props = {
-  bike: Bike;
+  id: Bike["id"];
+  title: Bike["title"];
+  year: Bike["year"];
+  price: Bike["price"];
+  image: Bike["image"];
 };
 
-function BikeCard({ bike }: Props) {
+function BikeCard({ id, title, year, price, image }: Props) {
+  const { addToCart } = useShoppingCart();
   const [modalOpen, setModalOpen] = useState(false);
-  const [selectedBike, setSelectedBike] = useState<Bike | null>(null);
   const imgRef = useRef<HTMLImageElement>(null);
-  const priceFormatted = formatCurrency(bike.price);
+  const priceFormatted = formatCurrency(price);
 
-  const onClickPicHandler = useCallback((bike: Bike | null) => {
+  const onClickPicHandler = useCallback(() => {
     const isPlaceholder = /placeholder_bike.svg/i.exec(imgRef.current?.src!);
 
     if (!isPlaceholder) {
-      setSelectedBike(bike);
-      setModalOpen((current) => !current);
-      lockScrollbar(!!bike);
+      setModalOpen((current) => {
+        lockScrollbar(!current);
+        return !current;
+      });
     }
   }, []);
 
@@ -27,22 +33,24 @@ function BikeCard({ bike }: Props) {
     <>
       <Container>
         <img
-          src={bike.image}
-          alt={`Pic of ${bike.title}`}
+          src={image}
+          alt={`Pic of ${title}`}
           ref={imgRef}
-          onClick={() => onClickPicHandler(bike)}
-          onError={(event) => onErrorImgHandler(event)}
+          onClick={onClickPicHandler}
+          onError={onErrorImgHandler}
         />
-        <h1>{bike.title}</h1>
+        <h1>{title}</h1>
         <Data>
-          <h3>{bike.year}</h3>
+          <h3>{year}</h3>
           <span>{priceFormatted}</span>
         </Data>
-        <AddButton>+ Add To Cart</AddButton>
+        <AddButton onClick={() => addToCart({ id, title, year, price, image })}>
+          + Add To Cart
+        </AddButton>
       </Container>
       {modalOpen && (
-        <Modal onClick={() => onClickPicHandler(null)}>
-          <img src={selectedBike?.image} alt="Bike Pic" />
+        <Modal onClick={onClickPicHandler}>
+          <img src={image} alt="Bike Pic" />
         </Modal>
       )}
     </>
